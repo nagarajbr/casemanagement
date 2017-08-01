3f8819b0fac01a485370ca92c79538acacd6849e
Nagaraj Bettadapura <nagaraj.bettadapura@gmail.com>
2017-07-31 19:43:12 -0500

    Fixed bug preventing access to citizenship forms

--- app/forms/citizenship_form.rb
+++ app/forms/citizenship_form.rb
@@ -1,14 +1,20 @@
+#### 07-31-2017, MB
+#### Added require statements to fix uninitialized constant CitizenshipForm::DSL error
+require 'capybara/dsl'
+require 'uber/options'
+
 class CitizenshipForm < Reform::Form
-		# include DSL
-		include Composition
-	    include Reform::Form::ActiveModel
+	include Capybara::DSL
+	include Composition
+	include Reform::Form::ActiveModel
+	
+	#### Removed symbols from array to fix [:citizenship, :sves_type] is not a symbol nor a string error
+	properties :citizenship, :sves_type, on: :client
+	properties :non_citizen_type, :country_of_origin,:refugee_status,:alien_DOE,:alien_no, on: :alien
 
-	    properties [:citizenship, :sves_type], on: :client
-	    properties [:non_citizen_type, :country_of_origin,:refugee_status,:alien_DOE,:alien_no], on: :alien
-
-	    model :client, on: :client
+	model :client, on: :client
 
 
-        validates_presence_of :citizenship, message: "is required "
-	    #validates_presence_of :residency, message: "is required"
-	end
+	#### Fixed syntax, changed validates_presence_of to validates_uniqueness_of
+	validates_uniqueness_of :citizenship, message: "is required"
+end
